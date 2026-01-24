876bdf21eadf04b67f01c5b9fa8101f85b265e3b
Artur Kohara Guerra <guerraartur1@gmail.com>
2026-01-24 16:12:15 -0300

    :bulb: docs: Adiciona comentário

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Habilita Cors para que as requisições vindas do front-end não sejam barradas
   app.enableCors({
     origin: 'http://localhost:3000',
   });
